8bc7a29857e5aecef7941cfce72f4683b7d6ba60
Denys Bezmenov <denys.bezmenov@globallogic.com>
2021-07-05 17:49:58 +0300

    Now using payload hash as a tag for InfluxDB record

--- daemon/main.py
+++ daemon/main.py
@@ -17,14 +17,13 @@ if __name__ == "__main__":
 
     data = requests.post(simple_server_url, json={"ids": list(tags.keys())}).json()
 
-    inc = 0  # TODO: replace this workaround for influxdb with something more meaningful (needed so that influxdb won't replace points with same timestamp), e.g. give them different tags
-
     for result in data['results']:
-        inc += 100
         decrypt = tags[result['id']].decrypt_message(result['payload'])
         date_time = datetime.fromtimestamp(int(result['datePublished'])/1000)
         write_api.write(influxdb_db, influxdb_org, Point(result['id'])
-                        .field('latitude', decrypt['lat']).field("longitude", decrypt['lon'])
+                        .tag('report_id', str(hash(result['payload'])))
+                        .field('latitude', decrypt['lat'])
+                        .field("longitude", decrypt['lon'])
                         .field("tooltip", date_time.strftime("%d/%m/%Y %H:%M:%S"))
-                        .time(int(result['datePublished']) * 1000000 + inc))
+                        .time(int(result['datePublished']) * 1000000))
     write_api.flush()
